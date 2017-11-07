2df87d783868d3c754ff420c1901c5040f4f6572
misu94 <misu@usc.edu>
2017-11-06 19:57:51 -0800

    diamond data

---
+++ diamonds/app.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(ggplot2)
+#GUI
+ui <- fluidPage(
+  titlePanel("Diamonds Data"),
+  sidebarLayout(sidebarPanel (
+    helpText("This app is to visualize adiamonds dataset"),
+    textInput(inputId = "title",label = "Chart title",value = ""),
+    selectInput(inputId="pos",label = "Position",choices = list("stack","dodge"),
+                selected = "stack")
+    
+  ) ,
+                mainPanel(plotOutput(outputId = "plot"))
+  )
+)
+
+
+#Server
+server <- function(input,output){
+  output$plot=renderPlot({ggplot(diamonds, aes(x=cut,fill=clarity))+geom_bar(position=input$pos)+
+      ggtitle(input$title)})
+}
+
+#Run the App
+shinyApp(ui,server)
+
+
